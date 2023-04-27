161428b28eecb508caee1958316889f80efa082f
Benjamin VIELLARD <gbicou@gmail.com>
2023-04-27 16:27:45 +0200

    style: eslint + prettier

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import type {Options} from "semantic-release";
+import type { Options } from "semantic-release";
 
 const config: Options = {
-    "branches": [
-        "+([0-9])?(.{+([0-9]),x}).x",
-        "main",
-        "next",
-        "next-major",
-        {"name": "beta", "prerelease": true},
-        {"name": "alpha", "prerelease": true}
-    ],
-    "plugins": [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/npm",
-        "@semantic-release/git",
-        "@semantic-release/github"
-    ]
-}
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
+};
 
-export default config
+export default config;
